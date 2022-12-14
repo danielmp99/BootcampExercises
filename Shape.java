609963bef6a9b702734c07a74e4af1c2cfc0a297
danielmp99 <82353415+danielmp99@users.noreply.github.com>
2022-12-13 19:59:55 -0500

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,59 @@
+package code.bootcamp.example;
+
+public class Shape {
+
+    public static void main(String[] args) {
+        int r = (int) 2.5;
+        System.out.println(rhombus(3));
+
+    }
+
+    public static String square() {
+        return square(3);
+    }
+
+    public static String square(int size) {
+        var character = "*";
+        var square = new StringBuilder();
+        for (int row = 1; row <= size; ++row) {
+            square.append(character.repeat(size)).append(System.lineSeparator());//String.format("%n")
+        }
+        System.out.println(square);
+        return square.toString();
+    }
+
+    public static String pyramid() {
+        return pyramid(3);
+    }
+
+    public static String pyramid(int size){
+        var pyramid = new StringBuilder();
+        var character = "*";
+        var space = " ";
+        for (int row = 1; row <= size; ++row) {
+            pyramid.append(space.repeat(size - row)).append(character.repeat(1 + (2 * (row - 1)))).append(System.lineSeparator());
+        }
+        return pyramid.toString();
+    }
+
+    public static String rhombus(){
+        return rhombus(5);
+    }
+
+    public static String rhombus(int size){
+        var rhombus = new StringBuilder();
+        var character = "*";
+        var space = " ";
+        for (int topRow = 1; topRow <= ((int) size/2) + 1; topRow++) {
+            rhombus.append(space.repeat((int) (size/2) - topRow + 1)).append(character.repeat(1 + (2 * (topRow - 1)))).append(System.lineSeparator());
+        }
+
+        System.out.println(rhombus);
+
+        for (int bottomRow = 1; bottomRow <= ((int) size/2); ++bottomRow) {
+            rhombus.append(space.repeat(bottomRow)).append(character.repeat(size - (2 * bottomRow))).append(System.lineSeparator());
+        }
+        return rhombus.toString();
+    }
+
+}
